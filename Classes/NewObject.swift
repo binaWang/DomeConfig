91d8ef026ca6c77841a19aaae3323d07248819a1
Wang, bin <b.c.wang@accenture.com>
2020-08-19 17:47:47 +0800

    测试开源库

---
+++ Classes/NewObject.swift
@@ -0,0 +1,13 @@
+//
+//  NewObject.swift
+//  DomeConfig
+//
+//  Created by Wang, bin on 2020/8/19.
+//  Copyright © 2020 Wang, bin. All rights reserved.
+//
+
+import UIKit
+
+class NewObject: NSObject {
+
+}
